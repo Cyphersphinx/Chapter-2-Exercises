3d10b6b3d26a762ff250a370bba1f5a3e029c9e4
Michael Reyes <michaelreyes808@gmail.com>
2019-01-13 17:07:48 -0700

    Updated more exercises 7-9

--- Chapter 2 Exercises/Chapter 2 Exercises/pch.cpp
+++ Chapter 2 Exercises/Chapter 2 Exercises/pch.cpp
@@ -3,6 +3,7 @@
 #include "pch.h"
 #include <iostream>
 #include <string>
+#include <math.h>
 
 // In general, ignore this file, but keep it around if you are using pre-compiled headers.
 
@@ -86,17 +87,72 @@ void Exer_2_6()
 
 void Exer_2_7()
 {
+	float userdata = 0.00;
+	int roundedNumber = 0;
 
+	std::cout << "Please enter a value to be rounded: ";
+	std::cin >> userdata;
+
+	roundedNumber = (int)round(userdata);
+
+	std::cout << "The value - " << userdata << " - rounded to the nearest integer is: " << roundedNumber;
 }
 
 void Exer_2_8()
 {
+	const int SECRET = 11;
+	int num1, num2, newNum;
+	
+	const double RATE = 12.50;
+	double hoursWorked, wage;
+		
+	std::string name ="";
+
+	std::cout << "Please enter two integer values (separated by a space): ";
+	std::cin >> num1 >> num2;
+	std::cout << "The value of num1 = " << num1 << " and the value of num2 = " << num2;
+
+	newNum = (num1 * 2) + num2;
+	std::cout << "The value of (num1 * 2) + num2 = " << newNum;
+
+	newNum += SECRET;
+
+	std::cout << "The value of newNum + SECRET = " << newNum;
+
+	std::cout << "\nEnter a person's last name: ";
+	std::cin >> name;
+
+	std::cout << "\nPlease enter a decimal number that is between 0 and 70: ";
+	std::cin >> hoursWorked;
+	wage = hoursWorked * RATE;
+
+	std::cout << "\nName: " << name;                  //output of the value of the variable name
+	std::cout << "\nPay Rate: " << RATE;              //output the value of RATE 
+	std::cout << "\nHours Worked: " << hoursWorked;     //output the value of the variable hoursWorked
+	std::cout << "\nSalary: " << wage;                  //output the value of the variable wages
+
+
 
 }
 
 void Exer_2_9()
 {
+	double testscores[5] = { 0.00 };
+	double avg = 0.00;
 
+	int arraySize;
+
+	std::cout << "Please enter five different test scores: ";
+	std::cin >> testscores[0] >> testscores[1] >> testscores[2] >> testscores[3] >> testscores[4];
+
+	arraySize = (sizeof testscores / sizeof testscores[0]);
+	for (int i = 0; i < arraySize ; i++)
+	{
+		avg += testscores[i];
+	}
+	
+	avg = avg / arraySize;
+	std::cout << "\nThe average of all " << arraySize << " test scores is: " << avg;
 }
 
 void Exer_2_10()
